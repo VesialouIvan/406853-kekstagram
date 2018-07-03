d4c9318ac0ea2781e69652621cf02cabc5cfcab6
VesialouIvan <vesialouivan@gmail.com>
2018-07-03 20:47:44 +0300

    бекэнд

--- js/preview.js
+++ js/preview.js
@@ -1,21 +1,53 @@
 'use strict';
 (function () {
 // Заполним блок .big-picture данными из первого элемента сгенерированного массива
+  var bigPicture = document.querySelector('.big-picture');
   var renderMainPost = function (mainPost) {
     var pictureSection = document.querySelector('.big-picture');
     var commentsList = pictureSection.querySelector('.social__comments');
+    var fragment = document.createDocumentFragment();
+    var comment;
 
     pictureSection.querySelector('.social__caption').textContent = mainPost.getRandomPostDescription;
-    pictureSection.querySelector('.big-picture__img img').src = mainPost.getRandomPostUrl;
-    pictureSection.querySelector('.likes-count').textContent = mainPost.getRandomPostLikes;
-    var comment = window.pictures.createCommentTemplate(mainPost.getRandomPostComments);
-    commentsList.appendChild(comment);
+    pictureSection.querySelector('.big-picture__img img').src = mainPost.url;
+    pictureSection.querySelector('.likes-count').textContent = mainPost.likes;
+    for (var i = 0; i < mainPost.comments.length; i++) {
+      comment = window.pictures.createCommentTemplate(mainPost.comments[i]);
+      fragment.appendChild(comment);
+    }
+
+    commentsList.appendChild(fragment);
+  };
+
+  var allPosts;
+
+  var onLoad = function (data) {
+    console.log(data);
+    var fragment = document.createDocumentFragment();
+    window.pictures.showPosts(window.data.picturesList, fragment, data);
+    allPosts = data;
   };
 
 
-  var allPosts = window.data.getRandomPosts(window.data.POSTS_COUNT);
-  var postsFragment = document.createDocumentFragment();
-  window.pictures.showPosts(window.data.picturesList, postsFragment, allPosts);
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+
+    node.style.margin = '0 auto';
+    node.style.textAlign = 'center';
+    node.style.backgroundColor = 'red';
+    node.style.position = 'relative';
+    node.style.padding = '20px 100px';
+    node.style.fontSize = '30px';
+    node.style.color = 'white';
+    node.textContent = errorMessage;
+    document.querySelector('.pictures').insertAdjacentElement('afterend', node);
+  };
+
+  window.backend.load(onLoad, onError);
+
+  // var allPosts = window.backend.load(onLoad, onError);
+  //var postsFragment = document.createDocumentFragment();
+  //window.pictures.showPosts(window.data.picturesList, postsFragment, allPosts);
 
   document.querySelector('.social__comment-count').classList.add('visually-hidden');
   document.querySelector('.social__loadmore').classList.add('visually-hidden');
@@ -34,8 +66,14 @@
     imgEditor.classList.add('hidden');
   });
 
-  window.preview = {
-    renderMainPost: renderMainPost,
-    allPosts: allPosts
-  };
+  // отображаем нужный пост по клику
+  window.data.picturesList.addEventListener('click', function (evt) {
+    // находим индекс картинки(data-index), которую мы кликнули
+      if (evt.target.getAttribute('data-index')) {
+        var target = evt.target;
+        var dataIndex = target.getAttribute('data-index');
+        bigPicture.classList.remove('hidden');
+        renderMainPost(allPosts[dataIndex]);
+      }
+    });
 })();
