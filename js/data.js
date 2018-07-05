298268aa4de902519b46abc90bee0ca257aacec1
VesialouIvan <vesialouivan@gmail.com>
2018-07-05 23:51:20 +0300

    module7-task1 travis

--- js/data.js
+++ js/data.js
@@ -2,15 +2,15 @@
 
 (function () {
   // запишем массивы
-  var PICTURE_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  var PICTURE_DESCRIPTIONS = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...',
-    'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
-  var MIN_LIKES = 15;
-  var MAX_LIKES = 200;
+  // var PICTURE_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
+  //   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  //   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  //   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  //   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  // var PICTURE_DESCRIPTIONS = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...',
+  //   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
+  // var MIN_LIKES = 15;
+  // var MAX_LIKES = 200;
   // var POSTS_COUNT = 25;
   var picturesList = document.querySelector('.pictures');
 
@@ -26,20 +26,20 @@
     return arr[Math.floor(Math.random() * arr.length)];
   };
 
-  var getPhotoUrl = function (index) {
-    var postUrl = 'photos/' + (index + 1) + '.jpg';
-    return postUrl;
-  };
+  // var getPhotoUrl = function (index) {
+  //   var postUrl = 'photos/' + (index + 1) + '.jpg';
+  //   return postUrl;
+  // };
 
   // получаем случайный пост
-  var createRandomPost = function (index) {
-    return {
-      getRandomPostUrl: getPhotoUrl(index),
-      getRandomPostLikes: getRandomNumber(MIN_LIKES, MAX_LIKES),
-      getRandomPostComments: getRandomElement(PICTURE_COMMENTS),
-      getRandomPostDescription: getRandomElement(PICTURE_DESCRIPTIONS)
-    };
-  };
+  // var createRandomPost = function (index) {
+  //   return {
+  //     getRandomPostUrl: getPhotoUrl(index),
+  //     getRandomPostLikes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+  //     getRandomPostComments: getRandomElement(PICTURE_COMMENTS),
+  //     getRandomPostDescription: getRandomElement(PICTURE_DESCRIPTIONS)
+  //   };
+  // };
 
   // получаем 25 случайных постов
   // var getRandomPosts = function (num) {
